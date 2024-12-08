eb5ffcd67d83ba132bf784fc68d395477f972310
Mat Jones <mat@mjones.network>
2024-12-08 10:00:51 -0500

    fix: configurable term program

--- flake.nix
+++ flake.nix
@@ -11,34 +11,37 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         shellPackages = with pkgs; [ godot_4 gdtoolkit_4 ];
-      in {
-        devShells.default = pkgs.mkShell { buildInputs = shellPackages; };
-        devShells.wezterm-and-nvim = let
-          godot_nvim = pkgs.writeShellScript "godot_nvim" ''
+        build_nvim_script = term_cmd: # bash
+          ''
             #!${pkgs.bash}/bin/bash
 
             server_path="$HOME/.cache/nvim/godot-server.sock";
             server_startup_delay=0.1 # delay in SECONDS, may need adjusted based on your nvim startup time
 
             start_server() {
-              wezterm -e $(which nvim) --listen "$server_path"
+              ${term_cmd} $(which nvim) --listen "$server_path"
             }
 
             open_file_in_remote_nvim() {
               # escape characters that nvim won't
               filename=$(printf %q "$1")
-              wezterm -e $(which nvim) --server "$server_path" --remote-send "<C-\><C-n>:n $filename<CR>:call cursor($2)<CR>"
+              ${term_cmd} $(which nvim) --server "$server_path" --remote-send "<C-\><C-n>:n $filename<CR>:call cursor($2)<CR>"
             }
 
             if ! [ -e "$server_path" ]; then
               # - start server FIRST then open the file
               start_server &
-              sleep $server_startup_delay # - wait for the server to start 
+              sleep $server_startup_delay # wait for the server to start
               open_file_in_remote_nvim "$1" "$2"
             else
               open_file_in_remote_nvim "$1" "$2"
             fi
           '';
+      in {
+        devShells.default = pkgs.mkShell { buildInputs = shellPackages; };
+        devShells.wezterm-and-nvim = let
+          godot_nvim =
+            pkgs.writeShellScript "godot_nvim" (build_nvim_script "wezterm -e");
         in pkgs.mkShell {
           buildInputs = shellPackages ++ [
             pkgs.coreutils # make sure you can `nohup godot &`
